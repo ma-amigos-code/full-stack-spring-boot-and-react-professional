9b52bc4d7937a1f67767aa7322c87b7d042e8144
Missael Aguilar <dev.missael@gmail.com>
2022-05-16 23:45:13 -0500

    First commit

---
+++ src/main/java/com/example/fullstackprofessional/FullStackProfessionalApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackprofessional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackProfessionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackProfessionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fullstackprofessional/FullStackProfessionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackprofessional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackProfessionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
